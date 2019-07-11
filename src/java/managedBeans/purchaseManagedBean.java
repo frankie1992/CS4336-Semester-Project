92478aa3ca086b1c39b7e28750105646f6e975be
frankie <francisco.miranda.92@gmail.com>
2019-07-11 14:47:37 -0500

    updated xhtml

--- src/java/managedBeans/purchaseManagedBean.java
+++ src/java/managedBeans/purchaseManagedBean.java
@@ -5,21 +5,108 @@
  */
 package managedBeans;
 
+import java.io.Serializable;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
-
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 /**
  *
  * @author Francisco
  */
 @Named(value = "purchaseManagedBean")
-@Dependent
-public class purchaseManagedBean {
-
-    /**
-     * Creates a new instance of purchaseManagedBean
-     */
+@SessionScoped
+public class purchaseManagedBean implements Serializable{
+    
+    private int tickets;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String CCnum;
+    private String zip;
+    
     public purchaseManagedBean() {
     }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCCnum() {
+        return CCnum;
+    }
+
+    public void setCCnum(String CCnum) {
+        this.CCnum = CCnum;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
     
+    public void validateCC(FacesContext context, UIComponent comp, Object value) {
+
+        String cc = (String) value;
+
+        if (cc.length() != 16) {
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage(
+                    "Invalid Credit Card");
+            context.addMessage(comp.getClientId(context), message);
+
+        }
+    }
+    
+    public void validateZip(FacesContext context, UIComponent comp, Object value) {
+
+        String cc = (String) value;
+
+        if (cc.length() != 5) {
+            ((UIInput) comp).setValid(false);
+
+            FacesMessage message = new FacesMessage(
+                    "Invalid Zipcode");
+            context.addMessage(comp.getClientId(context), message);
+
+        }
+    }
+    
+    public int totalCost(int tickets){
+        return tickets * 10;
+    }
 }
